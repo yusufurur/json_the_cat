c550d78c6a40e5b887d7a8d056c34688810b2ad6
yusufurur <yusufurur@gmail.com>
2023-02-18 15:31:11 -0500

    Breed Fetcher

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=sib`;
+
+  request(url, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('Breed not found', null);
+      return;
+    }
+    const breed = data(0);
+    callback(null, breed.Description);
+  });
+};
+
+module.exports = fetchBreedDescription;
